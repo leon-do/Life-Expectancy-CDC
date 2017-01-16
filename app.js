96dab6c9c28b9320a3ecb66dd2aafa9f1c5831d2
Leon Do <leondo123@gmail.com>
2017-01-15 23:20:47 -0500

    fuck

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ connection.connect();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 
 app.get('/', function(request, response) {
